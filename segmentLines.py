664cfd2d0334a9841b4db04076c532a992163c2e
John Joyce <johnjoyce@johns-mbp.dhcp.nd.edu>
2018-04-04 15:24:41 -0400

    major updates. graph algorithm. lets bang

--- segmentLines.py
+++ segmentLines.py
@@ -2,14 +2,14 @@
 import cv2
 from sklearn.externals import joblib
 from skimage.feature import hog
-from line import Line
+from line import LineImage
 import numpy as np
 
 # TODO make as general as possible
 # TODO add deskewing
 # TODO add line breaking support
 
-# Segments a single rectangle into multiple, may end up running on all lines
+# Segments a single rectangle into multiple, may end up running on all lines, probably should use histogram
 def segmentLinesFromRectangle(rect, img):
     return None
 
@@ -102,40 +102,41 @@ def segmentLinesFromImage(imageName):
 
     # 1. Grayscale
     im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-    cv2.imshow("grayscale initial", im_gray)
+    #cv2.imshow("grayscale initial", im_gray)
 
     # 2. Binarize
     ret, im_b = cv2.threshold(im_gray, PIXEL_THRESHOLD_1, 255, cv2.THRESH_BINARY) #adjust this.
 
     # 3. Blur
     im_blurred = cv2.GaussianBlur(im_b, (89, 5), 0)
-    cv2.imshow("blurred", im_blurred)
+    #cv2.imshow("blurred", im_blurred)
 
     # 4. Threshold
     ret, im_th = cv2.threshold(im_blurred, PIXEL_THRESHOLD_2, 255, cv2.THRESH_BINARY) #adjust this.
-    cv2.imshow("re-thresholded", im_th)
+    #cv2.imshow("re-thresholded", im_th)
 
     # 5. Blur
     im_reblurred = cv2.GaussianBlur(im_th, (51, 1), 0)
-    cv2.imshow("re-blurred", im_reblurred)
+    #cv2.imshow("re-blurred", im_reblurred)
 
     # 6. Threshold
     ret, im_reth = cv2.threshold(im_reblurred, PIXEL_THRESHOLD_3, 255, cv2.THRESH_BINARY) #adjust this.
-    cv2.imshow("rere-thresholded", im_reth)
+    #cv2.imshow("rere-thresholded", im_reth)
 
     # 7. Dilate
     dilation = cv2.dilate(im_reth,(11, 11),iterations = 6)
-    cv2.imshow("dilated", dilation)
+    #cv2.imshow("dilated", dilation)
 
     # 8. Erosion
     erosion = cv2.erode(dilation,(11, 11),iterations = 5)
-    cv2.imshow("erosion", erosion)
+    #cv2.imshow("erosion", erosion)
 
     # 9. Add whitespace border
     bordersize=20
     im_border=cv2.copyMakeBorder(erosion, top=bordersize, bottom=bordersize, left=bordersize, right=bordersize, borderType= cv2.BORDER_CONSTANT, value=255 )
     im_original_border=cv2.copyMakeBorder(im, top=bordersize, bottom=bordersize, left=bordersize, right=bordersize, borderType= cv2.BORDER_CONSTANT, value=(255, 255, 255) )
-    cv2.imshow("bordered", im_border)
+    #cv2.imshow("bordered", im_border)
+    im_gray_border=cv2.copyMakeBorder(im_gray, top=bordersize, bottom=bordersize, left=bordersize, right=bordersize, borderType= cv2.BORDER_CONSTANT, value=255 )
 
     # Find contours in image
     _, ctrs, hier = cv2.findContours(im_border.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
@@ -143,21 +144,27 @@ def segmentLinesFromImage(imageName):
     # Get rectangles contains each contour
     rects = [cv2.boundingRect(ctr) for ctr in ctrs]
     rects = processRects(rects, im_border)
-    print rects
+
+    lines = []
 
     # For each rectangular region, calculate HOG features and predict
     for rect in rects:
         print (rect)
         # Draw the rectangles
         adjustedHeight = int(rect[3] * RECT_HEIGHT_ADJUSTMENT_FACTOR) # 1.5 * height of rect
-        adjustedX = int(rect[1] - rect[3] * .25)
-        if (adjustedX < 0):
-            adjustedX = 0
+        adjustedY = int(rect[1] - 0.25 * rect[3])
+        if (adjustedY < 0):
+            adjustedY = 0
 
-        cv2.rectangle(im_border, (rect[0], adjustedX), (rect[0]+rect[2], adjustedX + adjustedHeight), (63, 191, 118), 2)
-        cv2.rectangle(im_original_border, (rect[0], adjustedX), (rect[0]+rect[2], adjustedX + adjustedHeight), (63, 191, 118), 2)
 
-    cv2.imshow("Resulting Image with Rectangular ROIs", im_border)
-    cv2.imshow("Output blobs with Rectangles", im_original_border)
+        # add Line to lines
+        lines.append(LineImage(im_gray_border[adjustedY:adjustedY+rect[3], rect[0]:rect[0]+rect[2]]))
 
-    cv2.waitKey()
+        cv2.rectangle(im_border, (rect[0], adjustedY), (rect[0]+rect[2], adjustedY + adjustedHeight), (63, 191, 118), 2)
+        #cv2.rectangle(im_original_border, (rect[0], adjustedY), (rect[0]+rect[2], adjustedY + adjustedHeight), (63, 191, 118), 2)
+
+    #cv2.imshow("Resulting Image with Rectangular ROIs", im_border)
+    #cv2.imshow("Output blobs with Rectangles", im_original_border)
+
+    #return lines to caller
+    return lines
